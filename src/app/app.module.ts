2464309c504584c95ea5d86121599fbe3977fd2d
CAMILLE <camille@pc-camille.lan>
2018-11-29 16:20:31 +0100

    Création premiers components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConcertComponent } from './concerts/concert/concert.component';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { ConcertsListComponent } from './concerts/concerts-list/concerts-list.component';
+import { PressComponent } from './presse/press/press.component';
+import { PressListComponent } from './presse/press-list/press-list.component';
+import { EnsembleComponent } from './vocalEnsemble/ensemble/ensemble.component';
+
+const routes: Routes = 
+[
+  {path: 'concert', component: ConcertComponent},
+]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcertComponent,
+    ConcertsListComponent,
+    PressComponent,
+    PressListComponent,
+    EnsembleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
